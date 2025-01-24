7261bd82ebdbf597bc75229f9520ff092c8cadc9
Pedro Emmanuel Buerger Da Silva <sombraios@hotmail.com>
2025-01-24 09:49:43 -0300

    adicionando mais extensões de img

--- app/lists/groups.py
+++ app/lists/groups.py
@@ -1,4 +1,4 @@
 excelgroup = [".xlsx", ".xls", ".csv"]
 txtgroup = [".txt"]
-imggroup = [".png", ".jpg"]
+imggroup = [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".svg", ".webp"]
 allgroups = excelgroup + txtgroup + imggroup
